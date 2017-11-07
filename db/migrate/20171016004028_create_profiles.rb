401f3b4e0de4804f90bf34a49c32afda3a203e39
Victor Hugo de Oliveira <victor-olliveira@live.com>
2017-11-07 10:08:14 +0000

    Add payment method to Profile.

--- db/migrate/20171016004028_create_profiles.rb
+++ db/migrate/20171016004028_create_profiles.rb
@@ -1,11 +1,12 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-      t.string :name
-      t.text :description
+      t.string :business_name
+      t.text   :description
       t.string :category
       t.string :address
       t.string :phone
+      t.string :payment_method
 
       t.timestamps null: false
       t.belongs_to :user, index: true
