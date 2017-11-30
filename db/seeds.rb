eda8dc3a1ac59a4f79db0959341aae876d1d6d00
Victor Hugo de Oliveira <victor-olliveira@live.com>
2017-11-30 16:48:59 +0000

    Fix C9's deletion bug on User's migration and add minor fixes on views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 Profile.create(business_name: "Pop Steak", description: "Primeiro Truck de Steaks and Burgers grelhados em chama aberta e fogo forte do Brasil!", category: "Food Truck", address: "Rua Halley Mestrinho, Ponta Negra - CEP: 59090-160, Natal/RN.", phone: "(84) XXXXX-XXXX", payment_method: "Dinheiro e cartão", popularity: 0)
 Profile.create(business_name: "BROWNIE MANIA", description: "1º Food Truck de Brownie do Brasil, Trabalhamos com grande variedade de brownies, derivados e Waffles.", category: "Food Truck", address: "Rua Halley Mestrinho, Ponta Negra - CEP: 59090-160, Natal/RN.", phone: "(84) XXXXX-XXXX", payment_method: "Dinheiro e cartão", popularity: 0)
 Profile.create(business_name: "Adoro Massa", description: "Food truck especializado em massas italiana.", category: "Food Truck", address: "Rua Ataulfo Alves, 1911 - Candelária, Natal/RN.", phone: "(84) XXXXX-XXXX", payment_method: "Dinheiro e cartão", popularity: 0)
